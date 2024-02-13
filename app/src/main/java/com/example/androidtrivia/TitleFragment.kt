429315d036f110933b279fa192b7e0f96e9d2348
Livia Carvalho <livia.carvalho.lb@gmail.com>
2024-02-13 13:19:37 -0400

    Navigation Graph created, along with Navigation Host Fragment in the Activity Layout

--- app/src/main/java/com/example/androidtrivia/TitleFragment.kt
+++ app/src/main/java/com/example/androidtrivia/TitleFragment.kt
@@ -6,18 +6,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
 import com.example.androidtrivia.databinding.FragmentTitleBinding
 
 
 class TitleFragment : Fragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val binding : FragmentTitleBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_title, container, false)
-        // returns the root of the layout we've just inflated
+        binding.btnPlay.setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment)
+        )
         return binding.root
     }
 }
