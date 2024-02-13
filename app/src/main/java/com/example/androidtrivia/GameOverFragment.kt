7420e838a30404ceb3e6cead0fbd8ea3bd46b4bd
Livia Carvalho <livia.carvalho.lb@gmail.com>
2024-02-13 09:37:22 -0400

    added GameOverFragment

---
+++ app/src/main/java/com/example/androidtrivia/GameOverFragment.kt
@@ -0,0 +1,23 @@
+package com.example.androidtrivia
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.androidtrivia.databinding.FragmentGameOverBinding
+
+class GameOverFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val binding : FragmentGameOverBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_game_over, container, false)
+        return binding.root
+    }
+
+}
