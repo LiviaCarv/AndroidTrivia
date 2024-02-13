8df448f16c3f671c9ed7b61eb841d8b226d7fa37
Livia Carvalho <livia.carvalho.lb@gmail.com>
2024-02-13 10:20:14 -0400

    added GameFragment layout and setQuestion method

--- app/src/main/java/com/example/androidtrivia/GameFragment.kt
+++ app/src/main/java/com/example/androidtrivia/GameFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.androidtrivia.databinding.FragmentGameBinding
 
 class GameFragment : Fragment() {
 
@@ -47,12 +50,29 @@ class GameFragment : Fragment() {
     private var questionIndex = 0
     private val numQuestions = 3
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_game, container, false)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val binding: FragmentGameBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_game, container, false)
+        randomizeQuestions()
+
+        // Bind this fragment class to the layout
+        binding.game = this
+
+
+    }
+
+    private fun randomizeQuestions() {
+        questions.shuffle()
+        questionIndex = 0
+        setQuestion()
+    }
+
+    private fun setQuestion() {
+        currentQuestion = questions[questionIndex]
+        answers = currentQuestion.answers.toMutableList()
+        answers.shuffle()
+        (activity as AppCompatActivity).supportActionBar?.title =
+            getString(R.string.title_android_trivia_question,questionIndex+1,numQuestions)
+
     }
 
 }
